523215cb69759bcaae879115ddbf904301563f89
崔仁军 <cuirenjun@aibaixun.com>
2022-01-13 17:21:57 +0800

    feat: 获取当前登录用户信息

--- baixun-basic/src/main/java/com/aibaixun/basic/constants/BsaeConst.java
+++ baixun-basic/src/main/java/com/aibaixun/basic/constants/BsaeConst.java
@@ -10,7 +10,7 @@ public class BsaeConst {
     /**
      * 从请求HEADER中获取token的前缀
      */
-    public static final String TOKEN_HEADER_NAME = "authorization";
+    public static final String TOKEN_HEADER_NAME = "authentication";
     /**
      * 从redis中获取token的前缀
      */
